282b5d7a3a2bf75337f5dd896dd876ff613ff9cd
Henrique Lira <henrique.melhado@hotmail.com>
2021-05-09 14:28:53 -0300

    DataTable integration

--- frontend/src/components/BarChart/index.tsx
+++ frontend/src/components/BarChart/index.tsx
@@ -64,7 +64,7 @@ function BarChart() {
     //    labels: {
     //        categories: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
     //   },
-    //   series: [
+    //   series:  [
     //        {
     //            name: "% Sucesso",
     //           data: [43.6, 67.1, 67.7, 45.6, 71.1]
